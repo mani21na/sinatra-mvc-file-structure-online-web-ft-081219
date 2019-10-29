e2ddb6ad5ce4add49c750709f855ff60a3405859
Hanna Lee <mani21na@gmail.com>
2019-10-29 04:39:49 +0000

    Done.

---
+++ app/models/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  attr_accessible :name, :breed, :age
+  
+  @@all = []
+  
+  def initialize (name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    
+    @@all << self
+  end 
+  
+  def self.all
+    @@all
+  end 
+end
